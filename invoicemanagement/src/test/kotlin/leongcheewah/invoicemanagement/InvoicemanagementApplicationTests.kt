f1fbdf0794047ba2e89d4b0cdd37c3a6eefcd40b
LeongCheeWah1991 <cheewah_1991@hotmail.com>
2023-02-19 22:31:23 +0800

    initial commit

---
+++ invoicemanagement/src/test/kotlin/leongcheewah/invoicemanagement/InvoicemanagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package leongcheewah.invoicemanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvoicemanagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
